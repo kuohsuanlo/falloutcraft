e8999f7c0e5470dedd7d90d0e4cfab8c4ce2803b
LogoCat <kuohsuanlo@gmail.com>
2016-04-11 05:14:46 +0800

    placeholderAPI
    falloutcraft_radiation
    falloutcraft_dehydration
    falloutcraft_fatigue

--- src/main/java/io/github/kuohsuanlo/falloutcraft/AddPlaceholder.java
+++ src/main/java/io/github/kuohsuanlo/falloutcraft/AddPlaceholder.java
@@ -11,6 +11,7 @@ public class AddPlaceholder extends EZPlaceholderHook{
 	  private final FalloutcraftPlayerListener listener;
 	
 	public AddPlaceholder(FalloutcraftPlugin instance) {
+		super(instance, "falloutcraft");
 		// TODO Auto-generated constructor stub
 		this.MainPlugin = instance;
 		this.listener = MainPlugin.playerListener;
@@ -18,10 +19,13 @@ public class AddPlaceholder extends EZPlaceholderHook{
 
 	@Override
 	public String onPlaceholderRequest(Player p, String identifier) {
+		 if (identifier.equals("radiation")) {
 	            return String.valueOf(MainPlugin.falloutstatsRadiation.get((p).getPlayerListName())+"/1000");
 	        }
+		 if (identifier.equals("dehydration")){
 			 return String.valueOf(MainPlugin.falloutstatsThirst.get((p).getPlayerListName())+"/1000");
 		 }
+		 if (identifier.equals("fatigue")){
 			 return String.valueOf(MainPlugin.falloutstatsFatigue.get((p).getPlayerListName())+"/1000");
 		 }
 	        // always check if the player is null for placeholders related to the player!
